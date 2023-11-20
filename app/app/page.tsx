80755cfcca453561555c0ce605cd3b0acd8af927
numumi <uma_is_shika@hotmail.co.jp>
2023-11-20 20:48:08 +0900

    update page  to fetch data

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,21 +1,33 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BoardCard from "./BoardCard";
-
-import boardListData from "../public/data/board-list.json";
 
 import { Board } from "../types/board-list";
 import { RecoilRoot } from "recoil";
 
+import axios from "axios";
+
 export default function Home() {
-  const [boardList, setBoardList] = useState<Board[]>(boardListData);
+  const [boardList, setBoardList] = useState<Board[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/");
+        setBoardList(response.data);
+      } catch (error) {
+        console.error("Error fetching board list:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
-
-      <div className="grid grid-cols-2 gap-4 p-4">
-        {boardList.map((board) => (
-          <BoardCard key={board.id} board={board} />
-        ))}
-      </div>
+    <div className="grid grid-cols-2 gap-4 p-4">
+      {boardList.map((board) => (
+        <BoardCard key={board.id} board={board} />
+      ))}
+    </div>
   );
 }
