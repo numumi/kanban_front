b6fdb81c91788f94b9cc22bf7806ea81c345808b
numumi <uma_is_shika@hotmail.co.jp>
2023-11-23 03:30:12 +0900

    fix fetchBoardData

--- app/recoils/selectors/fetchBoardData.ts
+++ app/recoils/selectors/fetchBoardData.ts
@@ -1,6 +1,6 @@
 import { selectorFamily } from "recoil";
 import axios from "axios";
-import BoardType from "@/types/board-data";
+import BoardType, { ColumnType } from "@/types/board-data";
 
 const fetchBoardData = selectorFamily<BoardType, number>({
   key: "fetchBoardData",
@@ -8,7 +8,18 @@ const fetchBoardData = selectorFamily<BoardType, number>({
     try {
       const url = `http://localhost:3000/board/${id}`;
       const response = await axios.get(url);
-      return response.data;
+      const data = {
+        ...response.data,
+        columns: response.data.columns.map((column: ColumnType) => ({
+          ...column,
+          id: `column-${column.id}`,
+          tasks: column.tasks.map((task) => ({
+            ...task,
+            id: `task-${task.id}`,
+          })),
+        })),
+      };
+      return data;
     } catch (error) {
       throw new Error("データの取得に失敗しました。");
     }
