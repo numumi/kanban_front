157edcc1fb4ebc25a619a716e3c8413e853ff203
numumi <uma_is_shika@hotmail.co.jp>
2024-02-12 22:04:15 +0900

    ユーザー認証追加

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -3,9 +3,5 @@ import { RecoilRoot } from "recoil";
 import BoardList from "./BoardList";
 
 export default function Home() {
-  return (
-    <RecoilRoot>
-      <BoardList />
-    </RecoilRoot>
-  );
+  return <BoardList />;
 }
