b701188f1516385aff9574df9b32abe9731297c4
numumi <uma_is_shika@hotmail.co.jp>
2024-03-02 11:28:06 +0900

    トップページの画像変更

--- app/app/Top.tsx
+++ app/app/Top.tsx
@@ -1,16 +1,23 @@
+import { useMediaQuery } from "react-responsive";
+
 export default function Top() {
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+
   const topStyle = {
-    padding: "50px",
-    backgroundImage: "url('/task_board.avif')",
+    padding: isMobile ? "20px" : "50px",
+    backgroundImage: isMobile
+      ? "url('/post-it-mobile.png')"
+      : "url('/post-it.jpg')",
     height: "100vh",
-    backgroundPosition: "center",
+    width: "100%",
     backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
     backgroundSize: "cover",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     color: "white",
-    fontSize: "3em",
+    fontSize: isMobile ? "2em" : "3em",
   };
 
   return <div style={topStyle}></div>;
