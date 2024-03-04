d6323b52124967c7ac4af28a36a83ae20f6a0540
numumi <uma_is_shika@hotmail.co.jp>
2024-03-04 21:21:15 +0900

    tailwindcss設定変更

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,20 +1,12 @@
 "use client";
-import { useEffect, useState } from "react";
 import BoardList from "../features/components/BoardList";
 import Top from "../components/Top";
 import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Home() {
-  const { user, isAuthenticated } = useAuth0(); // Replace with your login check logic
-  const [loading, setLoading] = useState(true);
+  const { isAuthenticated, isLoading } = useAuth0(); // Replace with your login check logic
 
-  useEffect(() => {
-    if (user !== undefined) {
-      setLoading(false);
-    }
-  }, [user]);
-
-  if (loading) {
+  if (isLoading && !isAuthenticated) {
     return <div>Loading...</div>; // ここにローディングスピナーなどを表示
   }
   return isAuthenticated ? <BoardList /> : <Top />;
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,11 +1,11 @@
-
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}", 
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./features/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
